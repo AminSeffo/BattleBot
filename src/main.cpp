9ea97c468d851a580e0b4b5e7fa671c4ef1f3d37
AminSeffo <amin.seffo@tum.de>
2023-11-22 21:42:33 +0100

    uploaded final main

--- src/main.cpp
+++ src/main.cpp
@@ -1,111 +1,127 @@
-/*
-  Arduino FS-I6X Demo
-  fsi6x-arduino-uno.ino
-  Read output ports from FS-IA6B receiver module
-  Display values on Serial Monitor
-  
-  Channel functions by Ricardo Paiva - https://gist.github.com/werneckpaiva/
-  
-  DroneBot Workshop 2021
-  https://dronebotworkshop.com
-*/
 #include <Arduino.h>
 #include <motorSetup.cpp>
-#define CH2 3
-#define CH1 5
-// Motor A
- 
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
- 
-// Motor B
- 
-int enB = 10;
-int in3 = 12;
-int in4 = 13;
- 
+#include <batteryManager.cpp>
+
+// Define channel constants for drive
+#define CH1 9  // right stick x-axis (left/right) - horizontal
+#define CH2 10 // right stick y-axis (up/down) - vertical
+
+// Define channel constants for weapon
+#define CH3 11  // switch on the top left of the controller
+#define CH5 3
  // Integers to represent values from sticks and pots
-int ch1Value;
-int ch2Value;
-Motor motor = {CH1, CH2, enA, enB, in1, in2, in3, in4};
+int ch3Value;
+int raw_values;
+int x;
+int y;
+int z;
+// Motor A
+int enA = 6;
+int in1 = 4;
+int in2 = 7;
+
+// Motor B
+int enB = 5;
+int in3 = 2;
+int in4 = 8;
+
+//  Weapon pin
+int weopon_pin = 12;
 
 
+// MotorDrive struct to hold motor setup information
+MotorDrive steering = {CH2, CH1, enA, enB, in1, in2, in3, in4};
+
+// MotorWeapon struct to hold motor setup information
+MotorWeapon weapon = {CH3, weopon_pin};
+
+/**
+ * Read the number of a specified channel and convert it to the given range.
+ * If the channel is off, return the default value.
+ *
+ * @param channelInput  The input pin for the channel
+ * @param minLimit      The minimum value for the output range
+ * @param maxLimit      The maximum value for the output range
+ * @param channelMin    The minimum pulse width for the channel when it's off
+ * @param channelMax    The maximum pulse width for the channel when it's on
+ * @param defaultValue  The value to return if the channel is off
+ *
+ * @return The mapped value within the specified range
+ */
+int readChannel(int channelInput, int minLimit, int maxLimit, int channelMin, int channelMax, int defaultValue) {
+  // ToDo:
+  // change 30000 to 25000
+  int ch = pulseIn(channelInput, HIGH, 35000);
+  //if (ch < 100) return defaultValue;
 
 
-
-
-
-// Read the number of a specified channel and convert to the range provided.
-// If the channel is off, return the default value
-
-
-// Read the number of a specified channel and convert to the range provided.
-// If the channel is off, return the default value
-int readChannel(int channelInput, int minLimit, int maxLimit, int channelMin,int channelMax, int defaultValue) {
-  int ch = pulseIn(channelInput, HIGH, 30000);
-  if (ch < 100) return defaultValue;
-  
   return map(ch, channelMin, channelMax, minLimit, maxLimit);
 }
-void setup(){
-  // Set up serial monitor
-  Serial.begin(115200);
-  
-  // Set all pins as inputs
-  pinMode(CH1, INPUT);
-  pinMode(CH2, INPUT);
-  // Set all the motor control pins to outputs
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-
+int readChannel_raw(int channelInput, int defaultValue) {
+  int ch = pulseIn(channelInput, HIGH, 35000);
+  //if (ch < 1000) return defaultValue;
+  return ch;
 }
-void demoOne(int speed)
+void setup() {
+  // Initialize the Serial Monitor
+  Serial.begin(9600);
+  // setup the battery manager
+  setupBatteryManager();
 
-{
-
-  // This function will run the motors in both directions at a fixed speed
-
-  // Turn on motor A
-
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-
-  // Set speed to 200 out of possible range 0~255
-
-  analogWrite(enA, speed);
-
-  // Turn on motor B
-
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-
-  // Set speed to 200 out of possible range 0~255
-
-  analogWrite(enB, speed);
-
+  // Setup motor control for drive
+  setupDrive(steering);
+  delay(1000);
+  // Setup motor control for weapon
+  setupWeapon(weapon);
 }
-
 
 void loop() {
+  // update the battery status
   
-  //demoOne();
-  //delay(1000);
-  // Get values for each channel
-  ch1Value = readChannel(CH1, -250, 250, 1001,1991, 0);
-  ch2Value = readChannel(CH2, -250, 250, 1004,1960, 0);
-  //ch2Value = readChannel(CH2, -250, 250, 0);
-  motorControl(motor,ch1Value,ch2Value);
+  //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // Read values from channels and map them to the desired range
+  x = readChannel(CH2, -500, 500, 1003, 1989, 0);
+  y = readChannel(CH1, -500, 500, 1001, 1989, 0);
+  ch3Value = readChannel(CH3, -500, 500, 995, 1989, 0);
+  x = constrain(x, -255, 255);
+  y = constrain(y, -255, 255);
+  z = constrain(ch3Value, -255, 255);
+  delay(50);
+
+  int right = y+x;
+  int left = y-x;
   
-  // Print to Serial Monitor
-  Serial.print("Ch1: ");
-  Serial.print(ch1Value);
-  Serial.print(" | Ch2: ");
-  Serial.print(ch2Value);
-  Serial.println();
-  delay(100);
+  int raw_value_2 =readChannel_raw(CH2, 0);
+
+  int raw_value_1 =readChannel_raw(CH1, 0);
+  int raw_value_3 =readChannel_raw(CH3, 0);
+  weoponControlnew(weapon, z);
+
+  arcSteering(steering, left, right);
+if (raw_value_1 == 0 && raw_value_2 == 0 && raw_value_3 == 0)
+{
+  turnMotorOff(steering);
+  turnWeaponOff(weapon);
+ }
+
+
+  //if (raw_value_3 > 1000){
+  //  weoponControl(weapon, z);
+  //  }
+ //if (raw_value_4 > 1000){
+ // x = readChannel(CH1, -200, 200, 1001, 1987, 0);
+ // y = readChannel(CH2, -200, 200, 1001, 1989, 0);
+ //  tankSteering(steering, x, y);  
+ // }
+  // print them all in one line
+
+  Serial.print("CH1: ");
+  Serial.print(raw_value_1);
+  Serial.print(" CH2: ");
+  Serial.print(raw_value_2);
+  Serial.print(" CH3: ");
+  Serial.print(raw_value_3);
+  Serial.print(" CH5: ");
+  Serial.println("");
+  
 }
